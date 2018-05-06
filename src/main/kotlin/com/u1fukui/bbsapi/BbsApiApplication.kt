fc90485d2b69d806f98b12284bd1213a3a654735
u1 <u1.kobayashi@gmail.com>
2018-05-06 16:03:16 +0900

    Create a project with Spring Initializr

---
+++ src/main/kotlin/com/u1fukui/bbsapi/BbsApiApplication.kt
@@ -0,0 +1,11 @@
+package com.u1fukui.bbsapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BbsApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<BbsApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/u1fukui/bbsapi/BbsApiApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.u1fukui.bbsapi
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class BbsApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
