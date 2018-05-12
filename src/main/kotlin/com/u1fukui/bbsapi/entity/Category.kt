8a4a8a0a6d428032fbaac070b6fbc91e8a442295
u1 <u1.kobayashi@gmail.com>
2018-05-12 13:59:06 +0900

    Create categories table

---
+++ src/main/kotlin/com/u1fukui/bbsapi/entity/Category.kt
@@ -0,0 +1,27 @@
+package com.u1fukui.bbsapi.entity
+
+import java.io.Serializable
+import java.time.LocalDateTime
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "categories")
+data class Category(
+    @Column(name = "name", nullable = false)
+    var name: String = "",
+    @Column(name = "created_at", nullable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now(),
+    @Column(name = "updated_at", nullable = false)
+    var updatedAt: LocalDateTime = LocalDateTime.now(),
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    var id: Long = 0
+) : Serializable {
+    constructor(categoryId: Long) : this(id = categoryId)
+}
